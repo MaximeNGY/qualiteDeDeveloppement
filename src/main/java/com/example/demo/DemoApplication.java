d5d1f04e5994195847cb1276e5b980c9151dc350
NGUYEN Maxime <12100361@crit-xubuntu>
2023-04-03 09:42:00 +0200

    Mise au point h2 conection et insertion 2 tuples

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,41 @@
 package com.example.demo;
 
+import com.example.demo.data.Voiture;
+import com.example.demo.data.VoitureRepository;
+import org.slf4j.ILoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	@Autowired
+	private VoitureRepository voitureRepository;
+
+	private static final Logger logger = LoggerFactory.getLogger(VoitureRepository.class);
+
+	public static void main(String[] args) { SpringApplication.run(DemoApplication.class, args);	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+
+//		Implémentation de l'interface CommandLineRunner
+//
+//			Permet d'éxécuter des instructions avant le lancement'
+
+		voitureRepository.save(new Voiture("Ford",59000));
+		voitureRepository.save(new Voiture("Nissan",4999));
+
+		//	Parcours de toutes les voitures de la base de données
+
+		/*for (Voiture v : VoitureRepository.findAll()){
+			Logger.info(v.getMarque() + "" + v.getPrix());
+		}*/
 	}
-	
 }
--- src/main/java/com/example/demo/data/VoitureRepository.java
+++ src/main/java/com/example/demo/data/VoitureRepository.java
@@ -1,7 +1,9 @@
 package com.example.demo.data;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface VoitureRepository extends CrudRepository<Voiture, Integer>{
 
 
